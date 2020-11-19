01543b3a83eed595bf3419ae282c12dc6b3dfe7d
sally al sardouk <sally.sardouk98@gmail.com>
2020-11-19 17:13:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ let walkToPark=function walkToPark(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
   return `Walk to the park with ${dogName} the ${dogBreed}`
 }
-let function throwFrisbee(dogName, dogBreed) {
+let throwFrisbee= function throwFrisbee(dogName, dogBreed) {
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
   return `Throw the frisbee for ${dogName} the ${dogBreed}`
 }
-function walkHome(dogName, dogBreed) {
+let walkHome=function walkHome(dogName, dogBreed) {
   console.log(`Walk home with ${dogName} the ${dogBreed}`);
   return `Walk home with ${dogName} the ${dogBreed}`;
 }
-function unleashDog(dogName, dogBreed) {
+let unleashDog=function unleashDog(dogName, dogBreed) {
   console.log(`UnleashDog ${dogName} the ${dogBreed}`);
   return `Unleash ${dogName} the ${dogBreed}`
 }
